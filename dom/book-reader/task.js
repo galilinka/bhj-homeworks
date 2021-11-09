fa688eacc831f5923c28238ec7cbe904dc1d2654
galilinka <gali_82@mail.ru>
2021-11-09 18:48:27 +0300

    second variables

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -4,15 +4,21 @@ const fontSize = document.querySelectorAll('.font-size');
 const fontSizeSmall = document.querySelector('.font-size_small');
 const fontSizeBig = document.querySelector('.font-size_big');
 const book = document.querySelector('.book');
+//const a = document.querySelector("a");
+//console.log(a.dataset.dataSize);
+
 
 fontSize.forEach(size => {
-    size.onclick = () => {
-        fontSize.classList.add('font-size_active');
-        if (fontSizeBig) {
-            book.classList.add('book_fs-big');
-        } else if (fontSizeSmall) {
-            book.classList.add('book_fs-small');
+    size.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        fontSize.forEach(font => {
+            if (font.classList.contains("font-size_active")) {
+              font.classList.remove("font-size_active");
             }
-    }
+        });
+        
+    })
 })
+
 
