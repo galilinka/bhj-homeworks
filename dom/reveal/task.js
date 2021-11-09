7e5d1d266f081812ce2881375a0339df0bb1e5a9
galilinka <gali_82@mail.ru>
2021-11-09 18:00:47 +0300

    second variables

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,36 +1,17 @@
 "use strict";
 
-// const reveal = document.querySelector('.reveal');
-// console.log(reveal);
+const reveal = document.querySelectorAll('.reveal');
+//console.log(reveal);
 
-// function getCoords(el) { 
-//     let box = el.getBoundingClientRect();
-//     alert(box);
-//     return box.top < window.innerHeight && box.bottom >= 0;
-// }
-
-const isInViewport = function (element) {
+document.addEventListener('scroll', function(element) {
     const viewportHeight = window.innerHeight;
     const elementTop = element.getBoundingClientRect().top;
-    return elementTop < viewportHeight ? true : false; 
-}; 
-const reveal = document.querySelector('.reveal');
-isInViewport(reveal);
-console.log(reveal);
+     
+    element.forEach(el, () => {
+        if (elementTop > 0 && elementTop < viewportHeight) {
+            reveal.classList.add('reveal_active');
+        } else reveal.classList.remove('reveal_active');
+    })
+    return elementTop;
+})
 
-    // reveal.addEventListener("scroll", function() {
-    //     const viewportHeight = window.innerHeight;
-    //     getCoords();
-
-    //     if (viewportHeight.classList.contains(element)) {
-    //         reveal.classList.add('reveal_active');
-    //     }
-        
-    //     return element < viewportHeight ? true : false;
-    // })
-
-
-// 1. получить размер box
-// 2. переделать на All 
-// 3. добавить цикл
-// 4. если есть во вьюпорт - active 
