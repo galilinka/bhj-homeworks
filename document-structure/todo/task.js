9ad5d2861b58d7a4b92f2a93378e86650c9eaed7
galilinka <gali_82@mail.ru>
2021-11-21 15:47:02 +0300

    second variables

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,29 +1,27 @@
 'use strict';
-
 const taskList = document.querySelector('.tasks__list');
 const input = document.querySelector('.tasks__input');
 const buttonTaskAdd = document.querySelector('.tasks__add');
 
-
 buttonTaskAdd.addEventListener('click', (e) => {
     e.preventDefault();
     if (input.value.trim() != '') {
-        let task = `<div class="task">
-        <div class="task__title">
-        ${input.value}
-        </div>
-        <a href="#" class="task__remove">&times;</a>
-      </div>`
-      taskList.innerHTML+=task;
+        let task = 
+            `<div class="task">
+                <div class="task__title">
+                    ${input.value}
+                </div>
+                <a href="#" class="task__remove">
+                    &times;
+                </a>
+            </div>`;
+      taskList.insertAdjacentHTML('beforeend', task);
+      let del = taskList.lastElementChild.querySelector('.task__remove');
+      del.addEventListener('click', e => {
+          e.preventDefault();
+          taskList.removeChild(del.parentElement);
+      });
       input.value = '';
-      const removes = document.querySelectorAll('.task__remove');
-      removes.forEach(remove => {
-        remove.addEventListener('click', (e) => {
-            e.preventDefault();
-            //console.log(remove);
-            taskList.removeChild(remove.parentElement);
-        })
-    })
     } else return;
 })
 
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,18 +2,32 @@
 
 const links = document.querySelectorAll(".has-tooltip");
 //console.log(tooltip);
+let current = 0;
 
 links.forEach((link, i) => {
     link.addEventListener("click", (e) => {
         e.preventDefault();
-        let tooltip = document.createElement("div");
-        tooltip.classList.add("tooltip");
-        tooltip.textContent = link.title;
-        if(link.nextElementSibling) {
-            link.parentElement.insertBefore(tooltip, link.nextElementSibling);
-        } else link.parentElement.append(tooltip);
-        
-        tooltip.classList.add("tooltip_active");
+        if(!link.nextElementSibling || 
+            !link.nextElementSibling.classList.contains('tooltip')) {
+                let y = link.getBoundingClientRect().top + 20;
+                let x = link.getBoundingClientRect().right - 
+                link.getBoundingClientRect().width;
+                let tooltip = `<div 
+                class="tooltip"
+                style="top: ${y.toFixed()}px; left: ${x.toFixed()}px;"
+            >
+                ${link.title}
+            </div>`;
+            link.insertAdjacentHTML('afterend', tooltip);
+            }
+            if (i != current) {
+                [...document.querySelectorAll('.tooltip')].find(e => {
+                    e.classList.contains('.tooltip_active') 
+                    && e.classList.remove('.tooltip_active');
+                })
+            }
+
+            link.nextElementSibling.classList.toggle('tooltip_active');
     })
 })
 
